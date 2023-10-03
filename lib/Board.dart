c989c885872e45de8bd6c43394a0e3fc5f016ea3
MadFlasheroo7 <jayeshseth45@gmail.com>
2023-10-04 00:21:11 +0530

    shhh...

--- lib/Board.dart
+++ lib/Board.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
+import 'dart:html';
+import 'dart:math';
 import 'package:clipboard/clipboard.dart';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:lottie/lottie.dart';
+import 'package:puzzle/Reward.dart';
 import 'package:puzzle/layout/responsive_layout_builder.dart';
 import 'widgets/Menu.dart';
 import 'widgets/MyTitle.dart';
@@ -12,7 +17,7 @@ class Board extends StatefulWidget {
   _BoardState createState() => _BoardState();
 }
 
-class _BoardState extends State<Board> {
+class _BoardState extends State<Board> with TickerProviderStateMixin {
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   int move = 0;
 
@@ -20,7 +25,7 @@ class _BoardState extends State<Board> {
   int secondsPassed = 0;
   bool isActive = false;
   Timer? timer;
-
+  final controller = ConfettiController(duration: 5.days);
   @override
   void initState() {
     super.initState();
@@ -30,10 +35,16 @@ class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     if (timer == null) {
       timer = Timer.periodic(duration, (Timer t) {
         startTime();
       });
+    }
+    if (isSorted(numbers)) {
+      controller.play();
+    } else {
+      controller.stop();
     }
 
     return SafeArea(
@@ -47,6 +58,7 @@ class _BoardState extends State<Board> {
         // ])),
         child: Stack(
           children: [
+
             Positioned(
                 right: 30,
                 bottom: 30,
@@ -58,6 +70,36 @@ class _BoardState extends State<Board> {
                   ),
                 )),
             Game(size),
+            Align(
+              alignment: Alignment.topLeft,
+              child: ConfettiWidget(
+                confettiController: controller,
+                // blastDirection: pi/2,
+                blastDirectionality: BlastDirectionality.explosive,
+                numberOfParticles: 50,
+                gravity: 0.1,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConfettiWidget(
+                confettiController: controller,
+                // blastDirection: pi/2,
+                blastDirectionality: BlastDirectionality.explosive,
+                numberOfParticles: 50,
+                gravity: 0.1,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: ConfettiWidget(
+                confettiController: controller,
+                // blastDirection: pi/2,
+                blastDirectionality: BlastDirectionality.explosive,
+                numberOfParticles: 50,
+                gravity: 0.1,
+              ),
+            ),
           ],
         ),
       ),
@@ -82,17 +124,6 @@ class _BoardState extends State<Board> {
                         fontSize: size.height * 0.1,
                         color: Colors.white,
                         decoration: TextDecoration.none),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Task 1",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: size.height * 0.05,
-                          color: Colors.white,
-                          decoration: TextDecoration.none),
-                    ),
                   ),
                   Image.asset(
                     "assets/images/Auto.png",
@@ -193,7 +224,7 @@ class _BoardState extends State<Board> {
   void checkWin() {
     final size = MediaQuery.of(context).size;
 
-    if (true) {
+    if (isSorted(numbers)) {
       isActive = false;
       showDialog(
           context: context,
@@ -201,61 +232,63 @@ class _BoardState extends State<Board> {
             return Dialog(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)), //this right here
-              child: Container(
-                height: MediaQuery.of(context).size.height / 1.2,
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Lottie.asset(
-                        'assets/images/animation_lmzd7s38.json',
-                        width: MediaQuery.of(context).size.width / 2,
-                        height: MediaQuery.of(context).size.height / 6,
-                      ),
-                      Text(
-                        "!!YOU WIN!!",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: size.height * 0.05,
-                            color: Colors.black,
-                            decoration: TextDecoration.none),
-                      ),
-                      Image.asset(
-                        "assets/images/key.png",
-                        width: MediaQuery.of(context).size.width / 3,
-                      ),
-                      // Text(
-                      //   "!!YOU WIN!!",
-                      //   style: TextStyle(
-                      //       fontWeight: FontWeight.bold,
-                      //       fontSize: size.height * 0.05,
-                      //       color: Colors.black,
-                      //       decoration: TextDecoration.none),
-                      // ),
-                      Spacer(),
-                      SizedBox(
-                        width: 220.0,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            FlutterClipboard.copy("23.022504FFFFFFFFFFF13E")
-                                .then((value) {
-                              return ScaffoldMessenger.of(context)
-                                  .showSnackBar(const SnackBar(
-                                content: Text('Key Copied'),
-                              ));
-                            });
-                            // Navigator.pop(context);
-                          },
-                          child: Text(
-                            "Copy Your Key",
-                            style: TextStyle(color: Colors.white),
-                          ),
+              child: Wrap(
+                children: [Container(
+                  // height: MediaQuery.of(context).size.height / 1.2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Lottie.asset(
+                          'assets/images/trophy.json',
+                          // controller: AnimationController(vsync: this, duration: 5.seconds),
+                          repeat: false,
+                          frameRate: FrameRate(60),
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.height / 6,
                         ),
-                      )
-                    ],
+                        // Text(
+                        //   "!!YOU WIN!!",
+                        //   style: TextStyle(
+                        //       fontWeight: FontWeight.bold,
+                        //       fontSize: size.height * 0.05,
+                        //       color: Colors.black,
+                        //       decoration: TextDecoration.none),
+                        // ),
+                        Image.asset(
+                          "assets/images/key.png",
+                          width: MediaQuery.of(context).size.width / 3,
+                        ),
+                        // Text(
+                        //   "!!YOU WIN!!",
+                        //   style: TextStyle(
+                        //       fontWeight: FontWeight.bold,
+                        //       fontSize: size.height * 0.05,
+                        //       color: Colors.black,
+                        //       decoration: TextDecoration.none),
+                        // ),
+                        // Spacer(),
+                        SizedBox(
+                          width: 220.0,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => Reward(),));
+                            },
+                            child: Text(
+                              "✨ Your Reward Awaits ✨"),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xff8859A1),
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(32.0)),
+                              minimumSize: Size(200, 50)),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
+                )],
               ),
             );
           });
---
+++ lib/Reward.dart
@@ -0,0 +1,123 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:random_text_reveal/random_text_reveal.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Reward extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF051215),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: [
+            ImageFiltered(
+              imageFilter: ImageFilter.blur(
+                sigmaX: 20,
+                sigmaY: 20,
+              ),
+              child: Image.asset('images/hex.png',
+                      fit: BoxFit.cover,
+                      color: Color(0xFF051215),
+                      width: double.infinity,
+                      height: double.infinity)
+                  .animate(onPlay: (controller) => controller.repeat())
+                  .shimmer(
+                      delay: 500.ms,
+                      duration: 4500.ms,
+                      color: Color(0xFF5603AD)),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child:
+                        Image.asset("assets/images/LogoWhite.png", width: 400)
+                            .animate(delay: 6.5.seconds)
+                            .shimmer(
+                                duration: 2.5.seconds,
+                                color: Color(0xFFFFD700)),
+                  ),
+                  GestureDetector(
+                      onLongPress: () async {
+                        final snackBar = SnackBar(
+                          content: Text("Key Copied"),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                        await Clipboard.setData(ClipboardData(
+                            text: "32332e30363733373339,37322e36303233373633"));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(50),
+                        child: FittedBox(
+                          fit: BoxFit.fill,
+                          child: Column(
+                            children: [
+                              RandomTextReveal(
+                                text: '32332e30363733373339',
+                                initialText: '૩૨૩૩૨e૩૩૨૩૩૨e૩૩૨૩૩૨e૩૩૨૩૩૨e૩',
+                                duration: Duration(seconds: 6),
+                                style: GoogleFonts.poppins(
+                                  fontSize: 36,
+                                  color: Colors.tealAccent,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                curve: Curves.easeIn,
+                              ),
+                              RandomTextReveal(
+                                text: '37322e36303233373633',
+                                duration: Duration(seconds: 6),
+                                style: GoogleFonts.poppins(
+                                  fontSize: 36,
+                                  color: Colors.tealAccent,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                curve: Curves.easeIn,
+                              ),
+                            ],
+                          ).animate(delay: 7.seconds).shimmer(
+                              duration: 2.5.seconds, color: Color(0xFFFFD700)),
+                        ),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                            "Hold The Key to Copy, You might have a 🕊 waiting for you",
+                            style: TextStyle(color: Colors.white))
+                        .animate(delay: 4.seconds)
+                        .fadeIn(duration: 2.5.seconds),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await Clipboard.setData(ClipboardData(
+                          text: "32332e30363733373339,37322e36303233373633"));
+                      launchUrl(Uri.parse(
+                          'https://drive.google.com/file/d/1YeNxMaeFqrh0mfeEWN-ZRXIeVZ-8gsh_/view?usp=sharing'));
+                    },
+                    child: Text("Claim your Reward 🎉"),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Color(0xFF5603AD),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                        minimumSize: Size(200, 50)),
+                  ).animate(delay: 5.seconds).fadeIn(duration: 2.5.seconds),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
